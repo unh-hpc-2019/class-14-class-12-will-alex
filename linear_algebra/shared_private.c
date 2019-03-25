b272c2c0c69b2abd27632be24b19127be390426d
Alexander Knysh <alex.knysh.u@gmail.com>
2019-03-25 13:29:09 -0400

    Optimization on/off.

--- linear_algebra/shared_private.c
+++ linear_algebra/shared_private.c
@@ -5,10 +5,10 @@ int
 main(int argc, char **argv)
 {
   int test = -1;
+  int thread_id;
+  int n_threads;
 #pragma omp parallel private(test, thread_id, n_threads)
   {
-    int thread_id = omp_get_thread_num();
-    int n_threads = omp_get_num_threads();
     printf("Hello %d/%d test=%d\n", thread_id, n_threads, test);
     test = thread_id;
   }
