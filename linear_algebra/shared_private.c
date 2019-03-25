fe38262b9a13921631ad26ab1726aca9b3af44ce
Alexander Knysh <alex.knysh.u@gmail.com>
2019-03-25 11:17:45 -0400

    Created and tested hello_openmp.c and shared_private.c.

---
+++ linear_algebra/shared_private.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "omp.h"
+
+int
+main(int argc, char **argv)
+{
+  int test = -1;
+#pragma omp parallel shared(test)
+  {
+    int thread_id = omp_get_thread_num();
+    int n_threads = omp_get_num_threads();
+    printf("Hello %d/%d test=%d\n", thread_id, n_threads, test);
+    test = thread_id;
+  }
+  printf("test = %d\n", test);
+}
