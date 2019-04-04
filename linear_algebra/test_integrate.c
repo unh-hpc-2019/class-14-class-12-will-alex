3409ce2409414622756a4baafc8d42b784f39729
William Feather <wgf1@fishercat.sr.unh.edu>
2019-04-04 00:29:12 -0400

    Parallelize test_integrate & avoid race
    in class exercise: First attempt to avoid the race condition
    
    [wgf1@fishercat build]$ OMP_NUM_THREADS=1 ./test_integrate
    sum = 0.785398, took: 5.35365 sec
    [wgf1@fishercat build]$ OMP_NUM_THREADS=4 ./test_integrate
    sum = 0.785398, took: 1.33992 sec
    [wgf1@fishercat build]$ OMP_NUM_THREADS=8 ./test_integrate
    sum = 0.785398, took: 0.67144 sec

--- linear_algebra/test_integrate.c
+++ linear_algebra/test_integrate.c
@@ -1,26 +1,36 @@
 
+#include "linear_algebra.h"
+
 #include <stdio.h>
 #include <math.h>
 
 static double
 f(double x)
 {
-  return sqrt(1. - x*x);
+  return sqrt(1. - x * x);
 }
 
 int
 main(int argc, char **argv)
 {
-  const int N = 1000;
+  const int N = 1E8;
   double dx = 1. / N;
 
+  double tbeg = Wtime();
   double sum = 0.;
-  for (int i = 0; i < N; i++) {
-    double x0 = i * dx;
-    double x1 = (i+1) * dx;
-    sum += .5 * (f(x0) + f(x1)) * dx;
+#pragma omp parallel
+  {
+    double local_sum = 0.;
+#pragma omp for
+    for (int i = 0; i < N; i++) {
+      double x0 = i * dx;
+      double x1 = (i + 1) * dx;
+      local_sum += .5 * (f(x0) + f(x1)) * dx;;
+    }
+    sum += local_sum;
   }
-  printf("sum = %g\n", sum);
+  double tend = Wtime();
+  printf("sum = %g, took: %g sec\n", sum, tend - tbeg);
 
   return 0;
 }
